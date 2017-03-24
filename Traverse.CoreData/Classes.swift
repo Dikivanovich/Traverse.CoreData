7c1dd3b6d37f249a3a708fe1709eb600b091f22d
Руслан Гасанович Кантулаев <kantulaev.ruslan@gmail.com>
2017-03-25 00:29:42 +0300

    Создание Station в Core Data, реализация в коде

---
+++ Traverse.CoreData/Classes.swift
@@ -0,0 +1,75 @@
+//
+//  Classes.swift
+//  Traverse
+//
+//  Created by Dik on 14.03.17.
+//  Copyright © 2017 Kantulaev Ruslan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Position {
+    var x: Double?
+    var y: Double?
+    var z: Double?
+}
+
+class CurrentData {
+    
+    
+    
+   func returnDate() -> String
+    {
+        let date = NSDate()
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "MM-dd-yyyy HH:mm"
+        let dateString = dateFormater.string(from: date as Date)
+        return dateString
+    }
+    var date: String = ""
+    
+    init() {
+     
+        self.date = returnDate()
+        
+    }
+    
+    
+}
+
+
+class StationTest {
+    
+    var dateInitStation: String
+    
+    var name: String
+    
+    var position = Position()
+    
+    
+    
+    init(name: String, dateInitStation: String) {
+        self.name = name
+        self.dateInitStation = dateInitStation
+        position = Position()
+        
+    }
+    
+    
+ 
+        
+    }
+    
+    
+    
+
+        
+
+    
+
+    
+
+
+    
+
